5afd792fb792d8f74cb3cc598ae49ea8438dd625
pauziah <pauziah@enablr.id>
2021-05-21 14:45:30 +0700

    List

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 # list
 
-# list ada
-list = [1, 2.2, 'pauziah']
-print(list[0])
+# Tipe Data pada Python - List, Slicing, Tuple, Set, dan Dictionary
+
+# LIST
+# list beda dengan array, list ini tidak harus memiliki tipe data yang sama
+list = [1, 2.2, 'pauziah', 'ziah']
+print(list[0]) # access index 0
+print(list[-1]) #access last index
+print(list[1:3]) #access index ke 1 dan 2 doang. 3 nya kaga
+
+# melakukan perubahan data element list
+ubahList = list[2]='ziah'
+print(ubahList)
+
+# penambahan list menggunakan append
+list.append(2)
+print(list)
+
+binatang = ['kucing', 'rusa', 'badak', 'gajah']
+# delete item list menggunakan del()
+del binatang[1]
+print(binatang)
+
