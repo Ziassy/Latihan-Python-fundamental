80ccdba1bd59322e53508292b94dfbf3b92e216e
pauziah <pauziah@enablr.id>
2021-05-21 16:14:39 +0700

    Dynamic Typing

---
+++ Dynamic Typing/main.py
@@ -0,0 +1,6 @@
+# dynamic typing, yakni bahasa pemrograman yang hanya mengetahui tipe variabel saat program berjalan dan dilakukan assignment
+
+# len() -> untuk mengetahui panjang string dan tidak berlaku untuk integer
+
+testLent = 'Pauziah'
+print(len(testLent))
